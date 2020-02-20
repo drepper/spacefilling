201a8433c28935884dc4e9fb8b7f0b87c8ad86e8
Ulrich Drepper <drepper@gmail.com>
2020-02-20 17:28:15 +0100

    Add __init__.py

--- draw.py
+++ draw.py
@@ -17,8 +17,8 @@ canvas.pack()
 
 
 for d in h256.range():
-	x,y = h256[d]
-	color = '#{:02x}{:02x}{:02x}'.format(d,d,d)
-	canvas.create_rectangle(x*scale,y*scale,(x+1)*scale,(y+1)*scale,fill=color)
+    x,y = h256[d]
+    color = '#{:02x}{:02x}{:02x}'.format(d,d,d)
+    canvas.create_rectangle(x*scale,y*scale,(x+1)*scale,(y+1)*scale,fill=color)
 
 tkinter.mainloop()
